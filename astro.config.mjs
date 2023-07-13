eb3e594bfce2182b0e1e86cea9a166d893054933
aroogit commit -m First <arek.dec@gmail.com>
2023-07-13 19:13:53 +0200

    added copress plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import partytown from "@astrojs/partytown";
 import image from "@astrojs/image";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arekdec.dev',
   experimental: {
     assets: true
   },
-  integrations: [partytown(), image()]
+  integrations: [partytown(), image(), compress()]
 });
